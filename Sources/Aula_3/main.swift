78b68954cf255c3a6354d37d0b895a5d22c6d079
Matheus Rodrigues <contato.matheusrodrigues3008@gmail.com>
2026-04-23 10:53:59 -0300

    .

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -1,3 +1,59 @@
 import Foundation
 
-print("Aula 3 de Swift!!")
+class Cofre {
+
+
+
+ private var saldo: Double
+
+
+
+ init() {
+
+ self.saldo = 0.0
+
+ }
+
+
+
+ public func depositar(quantia: Double) {
+
+ if quantia > 0 {
+
+ saldo += quantia
+
+ } else {
+
+ print("Ops! A quantia para depósito deve ser positiva.")
+
+ }
+
+ }
+
+ public func sacar(quantia: Double) {
+
+ if quantia <= 0 {
+
+ print("Ops! A quantia para saque deve ser positiva.")
+
+ } else if quantia > saldo {
+
+ print("Ops! Saldo insuficiente.")
+
+ } else {
+
+ saldo -= quantia
+
+ }
+
+ }
+
+
+
+ public func getSaldo() -> Double {
+
+return saldo
+
+ }
+
+}
